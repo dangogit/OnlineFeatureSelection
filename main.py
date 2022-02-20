9c534d280622cb1a2535bc22e1adc9411d5ce257
goldmand <49034106+goldmand@users.noreply.github.com>
2022-02-20 18:53:48 +0200

    init

---
+++ main.py
@@ -0,0 +1,44 @@
+import subprocess
+import os
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+moa_jar = os.path.join(this_dir, "moa-pom.jar")
+
+classifiers_db = {
+   "ABFS-NB": "EvaluatePrequential -l (meta.featureselection.FeatureSelectionClassifier -l bayes.NaiveBayes -s (newfeatureselection.BoostingSelector2 -g 100 -t 0.05 -D) -g 0.7 -m) -s (ArffFileStream -f input_file) -d output_file",
+   "ABFS-HT": "EvaluatePrequential -l (meta.featureselection.FeatureSelectionClassifier -s (newfeatureselection.BoostingSelector2 -g 100 -t 0.05 -D) -g 0.7 -m) -s (ArffFileStream -f input_file) -d output_file",
+   "ABFS-KNN": "EvaluatePrequential -l (meta.featureselection.FeatureSelectionClassifier -l (lazy.kNN -k 500) -s (newfeatureselection.BoostingSelector2 -g 100 -t 0.05 -D) -g 0.7 -m) -s (ArffFileStream -f input_file) -d output_file",
+   "NB": "EvaluatePrequential -l bayes.NaiveBayes -s (ArffFileStream -f input_file) -d output_file",
+   "KNN": "EvaluatePrequential -l (lazy.kNN -k 500) -s (ArffFileStream -f input_file) -d output_file",
+   "HT": "EvaluatePrequential -l (trees.HoeffdingTree -g 100) -s (ArffFileStream -f input_file) -d output_file"}
+
+data_db = {
+   "COVTYPE": os.path.join(this_dir, "covtype.arff"),
+   "IADS": "",
+   "NOMAO": "",
+   "PAMAP2": "",
+   "SPAM": ""}
+
+
+def run(moa_jar_path, command, input_path, output_path):
+    args = command.replace("input_file", input_path).replace("output_file", output_path)
+
+    cmd = f'java -cp {moa_jar_path} moa.DoTask {args}'
+
+    print(f"Running the command: {cmd}")
+    print(subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read())
+
+
+def run_simulations(num_of_tests):
+    for i in range(1, num_of_tests+1):
+        for classifier in classifiers_db.keys():
+            for data in data_db.keys():
+                if data_db[data] == "":
+                    continue
+                output_path = os.path.join(this_dir, f"{classifier}_{data}_{i}")
+                run(moa_jar, classifiers_db[classifier], data_db[data], output_path)
+
+
+run_simulations(1)
+
