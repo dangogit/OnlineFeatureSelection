804de477e46813bf2fb2996d55f22112949f7e37
samuelbenichou <benichoU11>
2022-02-21 18:21:18 +0200

    Add run_simulation1 func

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ def run_simulations(num_of_tests):
                     continue
                 output_path = os.path.join(this_dir, 'Results', f"{classifier}_{data}_{i}.csv")
                 run(moa_jar, sizeofag_jar, classifiers_db[classifier], data_db[data], output_path)
-                res_dict = pd.read_csv(output_path)
-                avg_correct = np.mean(res_dict['classifications correct (percent)'])
-                total_correct+=avg_correct
+                # res_dict = pd.read_csv(output_path)
+                # avg_correct = np.mean(res_dict['classifications correct (percent)'])
+                # total_correct+=avg_correct
 
 
 
