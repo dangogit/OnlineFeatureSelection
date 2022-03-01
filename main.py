e301a9c80de9eb2cd3b370dd1b467b02ac8e96ec
goldmand <49034106+goldmand@users.noreply.github.com>
2022-03-01 10:51:00 +0200

    added make dirs

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ sizeofag_jar = os.path.join(this_dir, "sizeofag-1.0.4.jar")
 
 classifiers_db = {
    "ABFS-NB": "EvaluatePrequential -l (meta.featureselection.FeatureSelectionClassifier -l bayes.NaiveBayes -s (newfeatureselection.BoostingSelector2 -g 100 -t 0.05 -D) -g 0.7 -m) -s (ArffFileStream -f input_file) -d output_file",
-   # "ABFS-HT": "EvaluatePrequential -l (meta.featureselection.FeatureSelectionClassifier -s (newfeatureselection.BoostingSelector2 -g 100 -t 0.05 -D) -g 0.7 -m) -s (ArffFileStream -f input_file) -d output_file",
-   # "ABFS-KNN": "EvaluatePrequential -l (meta.featureselection.FeatureSelectionClassifier -l (lazy.kNN -k 500) -s (newfeatureselection.BoostingSelector2 -g 100 -t 0.05 -D) -g 0.7 -m) -s (ArffFileStream -f input_file) -d output_file",
-   # "NB": "EvaluatePrequential -l bayes.NaiveBayes -s (ArffFileStream -f input_file) -d output_file",
-   # "KNN": "EvaluatePrequential -l (lazy.kNN -k 500) -s (ArffFileStream -f input_file) -d output_file",
-   # "HT": "EvaluatePrequential -l (trees.HoeffdingTree -g 100) -s (ArffFileStream -f input_file) -d output_file"
+   "ABFS-HT": "EvaluatePrequential -l (meta.featureselection.FeatureSelectionClassifier -s (newfeatureselection.BoostingSelector2 -g 100 -t 0.05 -D) -g 0.7 -m) -s (ArffFileStream -f input_file) -d output_file",
+   "ABFS-KNN": "EvaluatePrequential -l (meta.featureselection.FeatureSelectionClassifier -l (lazy.kNN -k 500) -s (newfeatureselection.BoostingSelector2 -g 100 -t 0.05 -D) -g 0.7 -m) -s (ArffFileStream -f input_file) -d output_file",
+   "NB": "EvaluatePrequential -l bayes.NaiveBayes -s (ArffFileStream -f input_file) -d output_file",
+   "KNN": "EvaluatePrequential -l (lazy.kNN -k 500) -s (ArffFileStream -f input_file) -d output_file",
+   "HT": "EvaluatePrequential -l (trees.HoeffdingTree -g 100) -s (ArffFileStream -f input_file) -d output_file"
 }
 
 data_db = {
@@ -55,6 +55,12 @@ def run_simulations(num_of_tests):
 
 
 def run_simulations1(num_of_tests):
+    if not os.path.exists(os.path.join(this_dir, 'Results')):
+        os.makedirs(os.path.join(this_dir, 'Results'))
+
+    if not os.path.exists(os.path.join(this_dir, 'Data', 'Shuffle')):
+        os.makedirs(os.path.join(this_dir, 'Data', 'Shuffle'))
+
     for classifier in classifiers_db.keys():
         for dataset_name in data_db.keys():
             if data_db[dataset_name] == "":
@@ -65,15 +71,15 @@ def run_simulations1(num_of_tests):
                     output_path = os.path.join(this_dir, 'Results', f"{classifier}_{dataset_name}_{i}.csv")
 
                     # Shuffle data:
-                    shuffle_output_path = os.path.join(os.path.dirname(data_db[dataset_name]), 'Shuffle', '{}_{}.arff'.format(dataset_name, i))
+                    shuffle_output_path = os.path.join(this_dir, 'Data', 'Shuffle', '{}_{}_{}.arff'.format(dataset_name, classifier, i))
                     arff_shuffle(input_path=data_db[dataset_name], relation=dataset_name, output_path=shuffle_output_path)
 
                     run(moa_jar, sizeofag_jar, classifiers_db[classifier], shuffle_output_path, output_path)
                     df = pd.read_csv(output_path)
                     total += df['classifications correct (percent)'].mean()
             result[classifier + '-' + dataset_name] = total/num_of_tests
+            print(result)
 
-    print(result)
     with open(os.path.join(this_dir, 'result.csv'), 'w') as f:
         for key in result.keys():
             f.write("%s, %s\n" % (key, result[key]))
